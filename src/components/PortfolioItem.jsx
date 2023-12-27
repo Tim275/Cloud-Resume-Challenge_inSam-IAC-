db2aaeab6fd357380c5fe2910809d7e8d1139d94
Tim275 <sagichdirnicht259@outlook.de>
2023-12-27 08:32:13 +0100

    test

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -9,10 +9,11 @@ function PortfolioItem({ title, imgUrl, stack, link }) {
          className="border-2 border-stone-900 dark:border-white rounded-md overflow-hidden"
       >
          <img
-            src={imgUrl}
-            alt="portfolio" 
-            className="w-full h-36 md:h-48 object-cover cursor-pointer"
-         />
+   src={imgUrl}
+   alt="portfolio" 
+   className="w-full h-36 md:h-48 cursor-pointer"
+   style={{ objectFit: "contain" }}
+/>
          <div className="w-full p-4">
             <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold ">{title}</h3>
             <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white ">
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,6 +1,6 @@
 export default [
    {
-      title: 'Cloud Resume Challenge',
+      title: 'Cloud Resume Challenge ',
       imgUrl: '/assets/Cloud-Resume.png',
       stack: ['React', 'Javascript', 'AWS'],
       link: 'https://github.com/Tim275/Cloud-Resume-Challenge',
@@ -11,10 +11,22 @@ export default [
       stack: ['React', 'Tailwind', 'AWS','Node'],
       link: 'https://github.com/Tim275/Serverless-TODO-APP-Frontend',
    },
+
+   {
+      title: 'Building a CI/CD Pipeline',
+      imgUrl: '/assets/devops1.png',
+      stack: ['DevOps',  'Jenkins', 'Terraform' ],
+      link: 'https://github.com/Tim275/News-Analys-with-AWS-Comprehend',
+   },
+
    {
       title: 'News analysis with AWS Comprehend',
       imgUrl: '/assets/newsanalysis.png',
       stack: ['Python', 'AWS'],
       link: 'https://github.com/Tim275/News-Analys-with-AWS-Comprehend',
    },
+
+ 
+
+
 ]
