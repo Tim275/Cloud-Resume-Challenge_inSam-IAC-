24516733c2130aa0dd3ce0521ffe43bf49241c8d
Tim275 <sagichdirnicht259@outlook.de>
2023-12-30 17:21:03 +0100

    image changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Timeline from './components/Timeline';
 
 function App() {
 	const [theme, setTheme] = useState(null);
-
+    
 	useEffect(() => {
 		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 			setTheme('dark');
@@ -74,7 +74,7 @@ function App() {
 		<div className="bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
 			<div className="max-w-5xl w-11/12 mx-auto">
 				<Intro />
-				<Portfolio />
+				<Portfolio theme={theme} /> 
 				<Timeline />
 				<Contact />
 				<Footer />
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,32 +1,50 @@
 export default [
    {
       title: 'Cloud Resume Challenge ',
-      imgUrl: '/assets/Cloud-Resume.png',
+      imgUrl:  {
+          light: '/assets/CloudResumehell.png',
+          dark: '/assets/CloudResumedark.png'
+      },
       stack: ['React', 'Javascript','AWS','CI/CD'],
       link: 'https://github.com/Tim275/Cloud-Resume-Challenge_inSam-IAC-',
+  },
+
+  {
+   title: 'Building a CI/CD Pipeline',
+   imgUrl: {
+       light: '/assets/devops1.png',
+       dark: '/assets/devops1.png'
    },
+   stack: ['Jenkins','Ansible','K8','Docker' ],
+   link: 'https://github.com/Tim275/News-Analys-with-AWS-Comprehend',
+},
+
+  {
+      title: 'Gitops and ArgoCD',
+      imgUrl: {
+          light: '/assets/GitOpshell.png',
+          dark: '/assets/GitOpsDARK.png'
+      },
+      stack: ['ArgoCD','Terraform','K8'],
+      link: 'https://github.com/Tim275/tetris-game',
+   },
+
    {
       title: 'Serverless TodoApp',
-      imgUrl: '/assets/Serverless_TODO.png',
+      imgUrl: {
+          light: '/assets/Serverless_TODO.png',
+          dark: '/assets/Serverless_TODO-dark.png'
+      },
       stack: ['React', 'Tailwind', 'AWS','Node'],
       link: 'https://github.com/Tim275/Serverless-TODO-APP-Frontend',
    },
-
-   {
-      title: 'Building a CI/CD Pipeline',
-      imgUrl: '/assets/devops1.png',
-      stack: [,'Jenkins','Terraform','K8','Docker' ],
-      link: 'https://github.com/Tim275/News-Analys-with-AWS-Comprehend',
-   },
-
    {
       title: 'News analysis with AWS Comprehend',
-      imgUrl: '/assets/newsanalysis.png',
+      imgUrl: {
+          light: '/assets/newsanalysis.png',
+          dark: '/assets/newsanalysis-dark.png'
+      },
       stack: ['Python', 'AWS'],
       link: 'https://github.com/Tim275/News-Analys-with-AWS-Comprehend',
    },
-
- 
-
-
-]
+];
