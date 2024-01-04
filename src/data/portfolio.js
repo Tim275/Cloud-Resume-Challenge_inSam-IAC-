8deffbf0f101227482cf92d20a33eb1d9ebd428e
Tim275 <sagichdirnicht259@outlook.de>
2024-01-04 15:19:25 +0100

    adjust2

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -9,7 +9,7 @@ export default [
       link: 'https://github.com/Tim275/Cloud-Resume-Challenge_inSam-IAC-',
   },
   {
-    title: 'Tetris Game Deployment with ArgoCD',
+    title: 'Tetris Deployment with ArgoCD',
     imgUrl: {
         light: '/assets/GitOpshell.png',
         dark: '/assets/GitOpsDARK.png'
