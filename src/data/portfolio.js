49cdb74ae9d39b2ef0e7b45181927cd3198db184
Tim275 <sagichdirnicht259@outlook.de>
2024-01-04 15:04:29 +0100

    swap

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -8,17 +8,6 @@ export default [
       stack: ['React', 'Javascript','AWS','CI/CD'],
       link: 'https://github.com/Tim275/Cloud-Resume-Challenge_inSam-IAC-',
   },
-
-  {
-    title: 'DevSecOps-Reddit-Clone',
-    imgUrl: {
-        light: '/assets/cicdgitops.drawio.png',
-        dark: '/assets/cicdgitopsdark.drawio.png'
-    },
-    stack: ['ArgoCD','Jenkins','K8',],
-    link: 'https://github.com/Tim275/Reddit-Clone-app',
- },
-
   {
     title: 'Gitops and ArgoCD',
     imgUrl: {
@@ -29,6 +18,17 @@ export default [
     link: 'https://github.com/Tim275/tetris-game',
  },
   {
+    title: 'DevSecOps-Reddit-Clone',
+    imgUrl: {
+        light: '/assets/cicdgitops.drawio.png',
+        dark: '/assets/cicdgitopsdark.drawio.png'
+    },
+    stack: ['ArgoCD','Jenkins','K8',],
+    link: 'https://github.com/Tim275/Reddit-Clone-app',
+ },
+
+
+  {
    title: 'Building a CI/CD Pipeline',
    imgUrl: {
        light: '/assets/devops1.png',
@@ -37,7 +37,6 @@ export default [
    stack: ['Jenkins','Ansible','K8','Docker' ],
    link: 'https://github.com/Tim275/News-Analys-with-AWS-Comprehend',
 },
-
 
 
    {
