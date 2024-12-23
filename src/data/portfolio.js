95eb0af4d6e765b54bda015ec737a6aae5e69525
Timour <timour@MacBook-Pro-von-Timour.local>
2024-12-23 11:35:59 +0100

    add project

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,132 +1,130 @@
 export default [
-   {
-      title: 'Cloud Resume Challenge ',
-      imgUrl:  {
-          light: '/assets/CloudResumehell.png',
-          dark: '/assets/CloudResumedark.png'
-      },
-      stack: ['React', 'Javascript','AWS','CI/CD'],
-      link: 'https://github.com/Tim275/Cloud-Resume-Challenge_inSam-IAC-',
+  {
+    title: "Cloud Resume Challenge ",
+    imgUrl: {
+      light: "/assets/CloudResumehell.png",
+      dark: "/assets/CloudResumedark.png",
+    },
+    stack: ["React", "Javascript", "AWS", "CI/CD"],
+    link: "https://github.com/Tim275/Cloud-Resume-Challenge_inSam-IAC-",
   },
 
   {
-    title: 'k8 3-Tier Architecture ',
-    imgUrl:  {
-        light: '/assets/k8hell.png',
-        dark: '/assets/k8dark.png '
+    title: "CI/CD with Gitlab and Helm  ",
+    imgUrl: {
+      light: "/assets/CICD_GITLAB_K8_HELM_HELL.png",
+      dark: "/assets/CICD_GITLAB_K8_HELM_DARK.png",
     },
-    stack: ['CI/CD', 'Terraform','Kubernetes'],
-    link: 'https://github.com/Tim275/kubernetes-3-tier-application',
-},
+    stack: ["Gitlab", "Helm", "k8", "ArgoCD"],
+    link: "https://gitlab.com/timour1/code-repo",
+  },
+  {
+    title: "k8 3-Tier Architecture ",
+    imgUrl: {
+      light: "/assets/k8hell.png",
+      dark: "/assets/k8dark.png ",
+    },
+    stack: ["CI/CD", "Terraform", "Kubernetes"],
+    link: "https://github.com/Tim275/kubernetes-3-tier-application",
+  },
 
   {
-    title: 'AWS 3-Tier Web Architecture',
-    imgUrl:  {
-        light: '/assets/3TierArch.png',
-        dark: '/assets/3TierArch.png'
-    },
-    stack: ['AWS', 'Terraform','MySQL'],
-    link: 'https://github.com/Tim275/aws-3-tier-web-architecture-IAC',
-},
-
-{
-    title: 'CDK Fullstack App',
+    title: "AWS 3-Tier Web Architecture",
     imgUrl: {
-        light: '/assets/FullstackCDKhell.png',
-        dark: '/assets/FullstackCDKdunkel.png'
+      light: "/assets/3TierArch.png",
+      dark: "/assets/3TierArch.png",
     },
-    stack: ['CDK','Typescript','CI/CD','AWS'],
-    link: 'https://github.com/Tim275/fullstack_Cdk-/tree/main',
- },
- {
-    title: 'DevSecOps-Reddit-Clone',
-    imgUrl: {
-        light: '/assets/cicdgitops.drawio.png',
-        dark: '/assets/cicdgitopsdark.drawio.png'
-    },
-    stack: ['ArgoCD','Jenkins','K8',],
-    link: 'https://github.com/Tim275/Reddit-Clone-app',
- },
+    stack: ["AWS", "Terraform", "MySQL"],
+    link: "https://github.com/Tim275/aws-3-tier-web-architecture-IAC",
+  },
 
-{
-    title: 'k8 Microservice Project',
-    imgUrl:  {
-        light: '/assets/ProjectArchitecture.png',
-        dark: '/assets/ProjectArchitecturedunkel.png# '
-    },
-    stack: ['Microservices','Kubernetes','Helm'],
-    link: 'https://github.com/Tim275/microservices-python-app',
-},
-
-
-{
-    title: 'CI/CD Pipeline for ECS',
-    imgUrl:  {
-        light: '/assets/cicdecrhell.png',
-        dark: '/assets/cicdecrdunkel.png   '
-    },
-    stack: ['AWS', 'CI/CD','Docker'],
-    link: 'https://github.com/Tim275/container_aws_ecs_ci_cd_pipeline.git',
-},
-
-
-
-
-{
-    title: 'genai-serverless-api',
-    imgUrl:  {
-        light: '/assets/bedrockhell.png',
-        dark: '/assets/bedrockdark.png   '
-    },
-    stack: ['AWS', 'Python','Serverless'],
-    link: 'https://github.com/Tim275/genai-serverless-api-backend?tab=readme-ov-file',
-},
-
-
-{
-    title: 'Event-Driven Store',
-    imgUrl:  {
-        light: '/assets/eventdrivenhell.png',
-        dark: '/assets/eventdrivendark.png'
-    },
-    stack: ['AWS', 'Node','Serverless','CI/CD'],
-    link: 'https://github.com/Tim275/event-driven-order-service-backend',
-},
-
-
-
-//tttt
   {
-    title: 'Tetris Deploy with ArgoCD',
+    title: "CDK Fullstack App",
     imgUrl: {
-        light: '/assets/GitOpshell.png',
-        dark: '/assets/GitOpsDARK.png'
+      light: "/assets/FullstackCDKhell.png",
+      dark: "/assets/FullstackCDKdunkel.png",
     },
-    stack: ['ArgoCD','Terraform','K8'],
-    link: 'https://github.com/Tim275/tetris-game',
- },
-
- {
-    title: 'CI/CD Pipelinve JAVA apps',
+    stack: ["CDK", "Typescript", "CI/CD", "AWS"],
+    link: "https://github.com/Tim275/fullstack_Cdk-/tree/main",
+  },
+  {
+    title: "DevSecOps-Reddit-Clone",
     imgUrl: {
-        light: '/assets/jenkinsmavenhell.png',
-        dark: '/assets/jenkinsmavendark.png'
+      light: "/assets/cicdgitops.drawio.png",
+      dark: "/assets/cicdgitopsdark.drawio.png",
     },
-    stack: ['ArgoCD','Jenkins','K8','Java'],
-    link: 'https://github.com/Tim275/Project3---java-maven-sonar-argocd-k8s',
- },
+    stack: ["ArgoCD", "Jenkins", "K8"],
+    link: "https://github.com/Tim275/Reddit-Clone-app",
+  },
 
+  {
+    title: "k8 Microservice Project",
+    imgUrl: {
+      light: "/assets/ProjectArchitecture.png",
+      dark: "/assets/ProjectArchitecturedunkel.png# ",
+    },
+    stack: ["Microservices", "Kubernetes", "Helm"],
+    link: "https://github.com/Tim275/microservices-python-app",
+  },
 
+  {
+    title: "CI/CD Pipeline for ECS",
+    imgUrl: {
+      light: "/assets/cicdecrhell.png",
+      dark: "/assets/cicdecrdunkel.png   ",
+    },
+    stack: ["AWS", "CI/CD", "Docker"],
+    link: "https://github.com/Tim275/container_aws_ecs_ci_cd_pipeline.git",
+  },
 
-  
+  {
+    title: "genai-serverless-api",
+    imgUrl: {
+      light: "/assets/bedrockhell.png",
+      dark: "/assets/bedrockdark.png   ",
+    },
+    stack: ["AWS", "Python", "Serverless"],
+    link: "https://github.com/Tim275/genai-serverless-api-backend?tab=readme-ov-file",
+  },
 
-   {
-      title: ' AWS Comprehend',
-      imgUrl: {
-          light: '/assets/newsanalysis.png',
-          dark: '/assets/newsanalysis.png'
-      },
-      stack: ['Python', 'AWS','Serverless'],
-      link: 'https://github.com/Tim275/News-Analys-with-AWS-Comprehend',
-   },
+  {
+    title: "Event-Driven Store",
+    imgUrl: {
+      light: "/assets/eventdrivenhell.png",
+      dark: "/assets/eventdrivendark.png",
+    },
+    stack: ["AWS", "Node", "Serverless", "CI/CD"],
+    link: "https://github.com/Tim275/event-driven-order-service-backend",
+  },
+
+  //tttt
+  {
+    title: "Tetris Deploy with ArgoCD",
+    imgUrl: {
+      light: "/assets/GitOpshell.png",
+      dark: "/assets/GitOpsDARK.png",
+    },
+    stack: ["ArgoCD", "Terraform", "K8"],
+    link: "https://github.com/Tim275/tetris-game",
+  },
+
+  {
+    title: "CI/CD Pipelinve JAVA apps",
+    imgUrl: {
+      light: "/assets/jenkinsmavenhell.png",
+      dark: "/assets/jenkinsmavendark.png",
+    },
+    stack: ["ArgoCD", "Jenkins", "K8", "Java"],
+    link: "https://github.com/Tim275/Project3---java-maven-sonar-argocd-k8s",
+  },
+
+  {
+    title: " AWS Comprehend",
+    imgUrl: {
+      light: "/assets/newsanalysis.png",
+      dark: "/assets/newsanalysis.png",
+    },
+    stack: ["Python", "AWS", "Serverless"],
+    link: "https://github.com/Tim275/News-Analys-with-AWS-Comprehend",
+  },
 ];
